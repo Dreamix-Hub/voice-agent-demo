db3a7193b178a9ef0a0c3ecb8c9a9fe0919bb623
Muhammad Abdullah <muhammad0abdullah68@gamil.com>
2026-07-09 16:23:30 +0500

    created Customer model

---
+++ backend/app/modules/customers/model.py
@@ -0,0 +1,27 @@
+from sqlalchemy import String, Text
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database.base_model import BaseModel
+
+
+class Customer(BaseModel):
+    __tablename__ = "customers"
+
+    name: Mapped[str] = mapped_column(String(100))
+
+    phone: Mapped[str] = mapped_column(
+        String(20),
+        unique=True,
+        index=True,
+    )
+
+    email: Mapped[str | None] = mapped_column(
+        String(255),
+        nullable=True,
+    )
+
+    notes: Mapped[str | None] = mapped_column(
+        Text,
+        nullable=True,
+    )
